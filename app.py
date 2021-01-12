7b419956eed64cf98443dad65b8b606748a5ef9b
Suryam Thapa <58198640+Yesm-crypto@users.noreply.github.com>
2021-01-13 03:27:47 +0545

    Updating the files!

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 import jsonify
 import requests
 import pickle
 import numpy as np
+import pandas as pd
 import sklearn
 import re
 from string import punctuation
@@ -15,8 +16,172 @@ w2v_model = pickle.load(open('word2vec_model.pkl', 'rb'))
 def Home():
     return render_template('index.html')
 
-@app.route("/predict", methods=['POST'])
+@app.route("/predict", methods=['GET'])
 def predict():
+    return render_template('predict.html')
+
+@app.route("/upload", methods=['GET'])
+def upload():
+    sample_file = pd.ExcelFile("sample.xlsx")
+    df = pd.read_excel(sample_file,"sample")
+    html = df.to_html()
+    return render_template('upload.html',sample_file=html)
+
+@app.route("/submit", methods=['POST'])
+def submit():
+    try:
+        if request.method=="POST":
+            global filename
+            file = request.files["file"]
+            margin = int(request.form["Margin"])
+            # print(margin)
+            # print(uploaded_file.filename)
+            try:
+                if file.filename.split(".")[-1]=="csv":
+                    # print("This is a csv file")
+                    df = pd.read_csv(file)
+                    raw_df = df.copy()
+                elif file.filename.split(".")[-1]=="xlsx":
+                    # print("This is an excel file")
+                    df = pd.read_excel(file.read())
+                    raw_df = df.copy()
+            except Exception as e:
+                sample_file = pd.ExcelFile("sample.xlsx")
+                df = pd.read_excel(sample_file,"sample")
+                html = df.to_html()
+                errors = f"""<h1 class="err"> Something is wrong with the file! <br> Please confirm whether the above criterias are fulfilled or not! </h1> <h4> Error: {e} </h4>"""
+                return render_template("upload.html",errors=errors,sample_file=html)
+            
+            # Checking the columns similarity
+            columns = raw_df.columns
+            ideal_cols = ["class","role","campaign type","average salary per annum","city","cost of living"]
+            similar_cols = True
+            for index, item in enumerate(ideal_cols):
+                if ideal_cols[index]!=columns[index]:
+                    similar_cols = False
+            if not similar_cols:
+                sample_file = pd.ExcelFile("sample.xlsx")
+                df = pd.read_excel(sample_file,"sample")
+                html = df.to_html()
+                errors = """<h1 class="err">Column headings should be exactly same!<br>Take this sample table as reference.</h1>"""
+                return render_template("upload.html",errors=errors,sample_file=html)
+            
+            #Dropping null values
+            raw_df = raw_df.dropna()
+            try:
+                # applying function to convert the rates into integers
+                raw_df["average salary per annum"] = raw_df["average salary per annum"].apply(conv_to_int)
+                # applying function to calculate hourly rates
+                raw_df["rph_feature"] = raw_df["average salary per annum"].apply(rate_calc)
+                # preprocessing the text data
+                raw_df["class"] = raw_df["class"].apply(preprocess)
+                raw_df["role"] = raw_df["role"].apply(preprocess)
+                raw_df["campaign type"] = raw_df["campaign type"].apply(preprocess)
+                raw_df["city"] = raw_df["city"].apply(preprocess)
+
+                types = raw_df["campaign type"].unique()
+                if len(types)>3:
+                    sample_file = pd.ExcelFile("sample.xlsx")
+                    df = pd.read_excel(sample_file,"sample")
+                    html = df.to_html()
+                    errors = f"""<h3>The model is trained on 3 campaign types(DTC,HCP and IC). <br>But in the uploaded file, there are {len(types)} campaign types. <br>  {', '.join(types).upper()} </h3>"""
+                    return render_template("upload.html",errors=errors,sample_file=html)
+
+                def dummy_hcp(types):
+                    hcp=["health care professional","health care professionals","hcp"]
+                    dtc=["direct to consumer","direct to consumers","dtc"]
+                    ic=["integrated communication","integrated communications","ic"]
+                    if types in hcp:
+                        return 1
+                    elif types in dtc:
+                        return 0
+                    elif types in ic:
+                        return 0
+            
+                def dummy_ic(types):
+                    hcp=["health care professional","health care professionals","hcp"]
+                    dtc=["direct to consumer","direct to consumers","dtc"]
+                    ic=["integrated communication","integrated communications","ic"]
+                    if types in hcp:
+                        return 0
+                    elif types in dtc:
+                        return 0
+                    elif types in ic:
+                        return 1
+                    
+                def dummy_ny(city):
+                    ny=["new york","New York","ny"]
+                    la=["los angeles","la","los angles"]
+                    ch=["chicago","Chicago","chi"]
+                    if city in ny:
+                        return 1
+                    elif city in la:
+                        return 0
+                    elif city in ch:
+                        return 0
+            
+                def dummy_la(city):
+                    ny=["new york","New York","ny"]
+                    la=["los angeles","la","los angles"]
+                    ch=["chicago","Chicago","chi"]
+                    if city in ny:
+                        return 0
+                    elif city in la:
+                        return 1
+                    elif city in ch:
+                        return 0
+                # dummy variables for campaign types
+                raw_df["health care professional"] = raw_df["campaign type"].apply(dummy_hcp)
+                raw_df["integrated communications"] = raw_df["campaign type"].apply(dummy_ic)
+                # dummy variables for cities
+                raw_df["new york"] = raw_df["city"].apply(dummy_ny)
+                raw_df["los angeles"] = raw_df["city"].apply(dummy_la)
+
+                # converting class and roles into vectors
+                raw_df["class"] = raw_df["class"].apply(modulus_of_wv)
+                raw_df["role"] = raw_df["role"].apply(modulus_of_wv)
+            except Exception as e:
+                sample_file = pd.ExcelFile("sample.xlsx")
+                df = pd.read_excel(sample_file,"sample")
+                html = df.to_html()
+                errors = f"""<h1 class="err"> Something is wrong with the data types of elements! <br> Please take this sample table as reference! </h1> <h4> Error: {e} </h4>"""
+                return render_template("upload.html",errors=errors,sample_file=html)
+
+            # Making final df for prediction
+            final_df = raw_df[["class","role","rph_feature",'health care professional', 'integrated communications',"new york",'los angeles','cost of living']]
+            # Let's have predictions
+            predictions = model.predict(final_df)
+            # Make a df out of predictions
+            pred_df = pd.DataFrame({"Predicted Rates":predictions})
+            # Function to calculate margin
+            def with_margin(x):
+                wm = x + int((margin/100)*x)
+                return wm
+            # Applying the function to calculate margin and creating a new column our of it
+            pred_df["Rates with Margin"] = pred_df["Predicted Rates"].apply(with_margin)
+            # Concatenating the dataframes
+            show_df = pd.concat((df,pred_df),axis=1)
+            # Returning the dataframe to the user
+            if file.filename.split(".")[-1]=="csv":
+                filename="rhpreds.csv"
+                show_df.to_csv(filename,index=False)
+            elif file.filename.split(".")[-1]=="xlsx":
+                filename="rhpreds.xlsx"
+                show_df.to_excel(filename,index=False)
+            html=show_df.head(10).to_html()
+            # df = pd.ExcelFile()
+            return render_template('download.html',sample_file=html)
+        else:
+            return render_template("index.html")
+    except Exception as e:
+            sample_file = pd.ExcelFile("sample.xlsx")
+            df = pd.read_excel(sample_file,"sample")
+            html = df.to_html()
+            errors = f"""<h1 class="err"> Something is wrong with the file! <br> Please take this sample table as reference! </h1> <h4> Error: {e} </h4>"""
+            return render_template("upload.html",errors=errors,sample_file=html)
+
+@app.route("/predictions", methods=['POST'])
+def predictions():
     Fuel_Type_Diesel=0
     if request.method == 'POST':
         try:
@@ -55,7 +220,7 @@ def predict():
             col = float(request.form['col'])
             # print(average_salary,rph_feature,col)
         except:
-            return render_template('index.html',salary_error_text="Sorry! Enter the labels in integer format!")
+            return render_template('predict.html',salary_error_text="Sorry! Enter the labels in integer format!")
 
 # "Class","Role","rph_feature",'health care professional', 'integrated communications',"New York",'Los Angles','Cost of living'        
         prediction=model.predict([[Class,Role,rph_feature,c_hcp,c_ic,City_Ny,City_La,col]])
@@ -63,11 +228,15 @@ def predict():
         margin=int(request.form['Margin'])
         output = output_1 + int((margin/100)*output_1)
         if output<0:
-            return render_template('index.html')
+            return render_template('predict.html')
         else:
-            return render_template('index.html',Class=c.capitalize(),Role = r.capitalize(),City=City,average_salary=average_salary,COL=col,output_1 = output_1,output=output,Type=Campaign)
+            return render_template('predict.html',Class=c.capitalize(),Role = r.capitalize(),City=City,average_salary=average_salary,COL=col,output_1 = output_1,output=output,Type=Campaign)
     else:
-        return render_template('index.html')
+        return render_template('predict.html')
+
+@app.route("/download-file/")
+def download():
+    return send_file(filename, attachment_filename=filename, as_attachment=True)
 
 # Preprocessing
 def preprocess(content):
@@ -77,8 +246,21 @@ def preprocess(content):
     text = re.sub(r'\d',' ',text)
     text = re.sub(r'\s+',' ',text)
     text = "".join([letter for letter in text if letter not in punctuation])
-    text = text.rstrip()
+    text = text.rstrip().lower()
     return text
+
+# convert the rates to integers
+def conv_to_int(x):
+    try:
+        t_n = x.rstrip().replace(",","").replace("$","")
+    except:
+        t_n = x
+    n = int(t_n)
+    return n
+# Calculation of hourly rates from yearly rates
+def rate_calc(x):
+    rate = int(int(x)/2080)
+    return rate
 
 # Finding the average feature vector representing the whole sentence
 num_features = 100
@@ -94,6 +276,7 @@ def avg_feature_vector(sentence, model, num_features, index2word_set):
     if (n_words > 0):
         feature_vec = np.divide(feature_vec, n_words)
     return feature_vec
+
 # Finding the modulus of word vectors
 def modulus_of_wv(words):
     global w2v_model
